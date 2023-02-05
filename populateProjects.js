b29bd5836f2202cb50fa13c6f25257cdab4590de
jonagull <jonathangullik@gmail.com>
2023-02-05 20:23:04 +0100

    added more styling and stuff

--- populateProjects.js
+++ populateProjects.js
@@ -3,19 +3,21 @@ import { projects } from "./projects.js";
 const projectsNode = document.getElementById("projects-parent");
 
 for (var i = 0; i < projects.length; i++) {
-    let projectContainer = document.createElement("div");
-    projectContainer.setAttribute("class", "project__container");
+    let projectContainer = document.createElement("a");
+    projectContainer.setAttribute("class", "card");
+
+    if (projects[i].link) {
+        projectContainer.setAttribute("href", projects[i].link);
+    }
 
     projectContainer.innerHTML = `
-    <div class="project-text__container">
-        <h1>${projects[i].title}</h1>
-        <p>${projects[i].description}</p>
-        <button>Link</button>
-    </div>
-
-    <div class="project-image__container">
-        <img src="${projects[i].image}" alt="">
-    </div>
+                <div class="card__background"
+                    style="background-image: url(${projects[i].image})">
+                </div>
+                <div class="card__content">
+                    <p class="card__category">${projects[i].title}</p>
+                    <h3 class="card__heading">${projects[i].description}</h3>
+                </div>
     `;
 
     projectsNode.appendChild(projectContainer);
--- projects.js
+++ projects.js
@@ -1,20 +1,26 @@
 export const projects = [
     {
         title: "DUK1",
-        description:
-            "Faucibus varius justo volutpat fermentum suspendisse nec auctor curabitur consequat quisque condimentum diam nunc egestas",
-        image: "https://preview.redd.it/web-design-with-midjourney-is-so-cool-v0-5wx886z1298a1.png?auto=webp&s=e2dca5af38c95cd006e334d2401adee8e58742a4",
+        description: "Faucibus varius justo volutpat fermentum suspendisse",
+        image: "https://i.pinimg.com/736x/7a/f0/8f/7af08fa2a102efe7c3e0e75ba867da1a.jpg",
+        link: "https://dukprod.no/",
     },
     {
         title: "DUK2",
-        description:
-            "Faucibus varius justo volutpat fermentum suspendisse nec auctor curabitur consequat quisque condimentum diam nunc egestas",
+        description: "Faucibus varius justo volutpat fermentum suspendisse",
         image: "https://preview.redd.it/web-design-with-midjourney-is-so-cool-v0-5wx886z1298a1.png?auto=webp&s=e2dca5af38c95cd006e334d2401adee8e58742a4",
+        link: "",
     },
     {
         title: "DUK3",
-        description:
-            "Faucibus varius justo volutpat fermentum suspendisse nec auctor curabitur consequat quisque condimentum diam nunc egestas",
+        description: "Faucibus varius justo volutpat fermentum suspendisse",
         image: "https://preview.redd.it/web-design-with-midjourney-is-so-cool-v0-5wx886z1298a1.png?auto=webp&s=e2dca5af38c95cd006e334d2401adee8e58742a4",
+        link: "",
+    },
+    {
+        title: "DUK4",
+        description: "Faucibus varius justo volutpat fermentum suspendisse",
+        image: "https://preview.redd.it/web-design-with-midjourney-is-so-cool-v0-5wx886z1298a1.png?auto=webp&s=e2dca5af38c95cd006e334d2401adee8e58742a4",
+        link: "",
     },
 ];
